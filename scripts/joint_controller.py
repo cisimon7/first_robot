cb4d00a19bf9b1d93f8deb052376ffd4e2a134ff
cisimon7 <simontaneous1@hotmail.com>
2020-09-10 22:25:31 +0100

    more joints being controlled

--- scripts/joint_controller.py
+++ scripts/joint_controller.py
@@ -12,10 +12,39 @@ if __name__ == '__main__':
 
     rate = rospy.Rate(20)
     new = 0
+    new1 = 0
+    new1_inc = True
+    new2 = 0
+    new2_inc = True
 
     while not rospy.is_shutdown():
         joint_state.header.stamp = rospy.Time.now()
         new += 0.01
-        joint_state.position = [new, 0, 0]
+
+        if new1 > 1.21:
+            new1_inc = False
+        elif new1 < 0.01:
+            new1_inc = True
+
+        if new1_inc:
+            new1 += 0.01
+            print(new1)
+        else:
+            print(new1)
+            new1 -= 0.01
+
+        if new2 > 1.04:
+            new2_inc = False
+        elif new2 < -2.04:
+            new2_inc = True
+
+        if new2_inc:
+            new2 += 0.01
+            print(new2)
+        else:
+            print(new2)
+            new2 -= 0.01
+
+        joint_state.position = [new, new1, new2]
         joint_pub.publish(joint_state)
         rate.sleep()
